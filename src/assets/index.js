b351df257aa4a20b978f6d2d296923c4c47fe0a7
HanuKoshti <hanukoshti13@gmail.com>
2024-08-31 19:16:02 +0530

    Update index.js
    removed logo2

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import logo2 from "./logo2.png";
+
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -34,7 +34,6 @@ import tripguide from "./tripguide.png";
 
 export {
   logo,
-  logo2,
   backend,
   creator,
   mobile,
