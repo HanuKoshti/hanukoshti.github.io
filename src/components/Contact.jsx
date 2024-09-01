d95c0b73a19f170adf3c05a66b48bdeaa4fba7cb
HanuKoshti <hanukoshti13@gmail.com>
2024-09-01 07:54:04 +0530

    Update Contact.jsx
    updated service template and public id of emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_a68kuxe",
+        "template_03ust4q",
         {
           from_name: form.name,
           to_name: "Hanu Koshti",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "hanukoshti13@gmai.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "JyttZ5AbrDnu7qQn6"
       )
       .then(
         () => {
